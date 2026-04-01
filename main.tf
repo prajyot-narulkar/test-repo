ae30f979dc537b4f5d30ece3e7439eaa3f8cfee9
prajyot-narulkar <prajyotprashant.narulkar@annalect.com>
2026-04-01 13:56:32 +0530

    Add Terraform configuration for null resource

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "null_resource" "hello" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "echo hello"
+  }
+}
